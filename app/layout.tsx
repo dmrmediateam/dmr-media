779a0ccaf76ef0b5d66b07d647b0c172de83508d
Andrew Rohm <team@dmrmedia.org>
2025-12-03 16:39:17 -0600

    feat: add Meta Pixel (Facebook Pixel) tracking to all pages
    - Add Meta Pixel code to root layout for site-wide tracking
    - Use Next.js Script component with afterInteractive strategy
    - Include noscript fallback image for users with JavaScript disabled
    - Pixel ID: 1985215999036951
    - Tracks PageView events on all pages automatically

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,35 @@ export default function RootLayout({
   return (
     <html lang="en" className={instrumentSerif.variable}>
       <body className={instrumentSerif.className}>
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1985215999036951');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1985215999036951&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <ScrollProgressBar />
         <ScrollAnimations />
         <Navbar />
